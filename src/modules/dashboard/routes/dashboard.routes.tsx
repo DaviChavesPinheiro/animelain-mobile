78b6fffbc70ba9364f7f66c05aee5f03de9b5565
Davi <davicpinheiro123@hotmail.com>
2021-01-04 15:16:15 -0300

    BottomTabBarNavigation: Style fixed

--- src/modules/dashboard/routes/dashboard.routes.tsx
+++ src/modules/dashboard/routes/dashboard.routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
 import Home from '../pages/Home';
 import Favorites from '../pages/Favorites';
@@ -8,10 +9,59 @@ import Profile from '../pages/Profile';
 const Dashboard = createBottomTabNavigator();
 
 const DashboardRoutes: React.FC = () => (
-  <Dashboard.Navigator>
-    <Dashboard.Screen name="Início" component={Home} />
-    <Dashboard.Screen name="Favoritos" component={Favorites} />
-    <Dashboard.Screen name="Perfil" component={Profile} />
+  <Dashboard.Navigator
+    tabBarOptions={{
+      style: {
+        backgroundColor: '#1e1e1e',
+        borderTopColor: '#1e1e1e',
+        height: 55,
+        paddingTop: 5,
+      },
+      activeTintColor: '#03a9f5',
+      labelStyle: {
+        marginBottom: 6,
+        marginTop: 4,
+        fontSize: 10,
+        fontFamily: 'Roboto-Medium',
+      },
+    }}
+  >
+    <Dashboard.Screen
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="compass" color={color} size={23} />
+        ),
+      }}
+      name="INÍCIO"
+      component={Home}
+    />
+    <Dashboard.Screen
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="heart" color={color} size={23} />
+        ),
+      }}
+      name="FAVORITOS"
+      component={Favorites}
+    />
+    <Dashboard.Screen
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="clock" color={color} size={23} />
+        ),
+      }}
+      name="RECENTES"
+      component={Favorites}
+    />
+    <Dashboard.Screen
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Feather name="user" color={color} size={23} />
+        ),
+      }}
+      name="PERFIL"
+      component={Profile}
+    />
   </Dashboard.Navigator>
 );
 
