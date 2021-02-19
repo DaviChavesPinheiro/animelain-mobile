0ba97117e634c1093f63ceb5dba806816d4118e1
Davi <davicpinheiro123@hotmail.com>
2021-02-19 09:47:00 -0300

    react-native-navigation-bar-color implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { color } from 'react-native-reanimated';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import { ApolloProvider } from '@apollo/client';
 import AppProvider from './shared/hooks';
 
@@ -11,6 +11,10 @@ import Routes from './shared/routes';
 import client from './shared/services/client';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    changeNavigationBarColor('#1e1e1e', false, true);
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
